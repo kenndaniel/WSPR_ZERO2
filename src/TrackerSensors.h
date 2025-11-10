d0435ad24e11e8e88f2b1642f181f6b559a9e9e9
kenndaniel <kenndaniel@yahoo.com>
2025-11-10 04:29:47 -0600

    Changed file name

---
+++ src/TrackerSensors.h
@@ -0,0 +1,59 @@
+//#define PICO
+
+
+#ifdef PICO
+
+void sensorSetup()
+{
+analogReadResolution(12);
+}
+
+#else
+#include <TemperatureZero.h>  // SAMD21 temperature Lib
+void sensorSetup() 
+{return;}
+#endif
+
+bool tempInit = false;
+
+float getTempCPU()
+{  // Return temperature in C
+
+float tempC = 0.;
+
+
+  #ifdef PICO
+  // processor internal temperature in C
+  tempC = analogReadTemp(3.3f);
+
+
+  #else // SAMD21
+
+  TemperatureZero TempZero = TemperatureZero();
+  TempZero.init();
+  tempC = TempZero.readInternalTemperature();
+  #endif
+
+    return tempC;
+}
+
+int readVcc() 
+{  
+// Return the solar panel volts in mV e.g. 3700
+  int result = 0.;
+  #ifdef PICO
+  analogReadResolution(12);
+  unsigned int sensorValue = analogRead(PANEL_VOLTS);
+  result = (100*29.42*sensorValue*3.3f) / 4095.;
+  Serial.print(" Voltage ===== ");
+  Serial.println(result);
+
+  #else // SAMD21
+
+	unsigned int sensorValue = analogRead(PANEL_VOLTS);
+  result = (sensorValue * 2940*3.3)/1032.;
+
+     #endif
+
+  return result;
+}
